ebf0f518bab74095f805f93454f731b76724b27b
Ivan Krstic <krstic8687@yahoo.com>
2022-11-24 16:45:08 +0100

    initial commit

---
+++ src/test/java/it/springframework/msscjacksonexample/MsscJacksonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.springframework.msscjacksonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJacksonExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
