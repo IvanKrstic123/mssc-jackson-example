5652775068f56c902840c60431111e86f6cacaad
Ivan Krstic <krstic8687@yahoo.com>
2022-11-24 16:58:51 +0100

    added json testing with spring boot - serialization and deserialization

---
+++ src/test/java/it/springframework/msscjacksonexample/model/BaseTest.java
@@ -0,0 +1,19 @@
+package it.springframework.msscjacksonexample.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+public class BaseTest {
+    BeerDto getDto() {
+        return BeerDto.builder()
+                .id(UUID.randomUUID())
+                .beerName("BeerName")
+                .beerStyle("Ale")
+                .upc(12313213123L)
+                .createdDate(OffsetDateTime.now())
+                .lastUpdatedDate(OffsetDateTime.now())
+                .price(new BigDecimal("12.00"))
+                .build();
+    }
+}
---
+++ src/test/java/it/springframework/msscjacksonexample/model/BeerDtoTest.java
@@ -0,0 +1,40 @@
+package it.springframework.msscjacksonexample.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@JsonTest
+class BeerDtoTest extends BaseTest{
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    /**
+     * serializing json
+    * */
+    @Test
+    void testSerializeDto() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonString = objectMapper.writeValueAsString(beerDto);
+
+        System.out.println(jsonString);
+    }
+
+    /**
+     * deserializing json
+    **/
+    @Test
+    void deserializeBeerDto() throws JsonProcessingException {
+        String json = "{\"id\":\"d1a22f1c-3b02-4dde-8ea2-daccc85675aa\",\"beerName\":\"BeerName\",\"beerStyle\":\"Ale\",\"upc\":12313213123,\"price\":12.00,\"createdDate\":\"2022-11-24T16:55:27.0365084+01:00\",\"lastUpdatedDate\":\"2022-11-24T16:55:27.0385088+01:00\"}\n";
+
+        BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
+
+        System.out.println(beerDto);
+    }
+}
